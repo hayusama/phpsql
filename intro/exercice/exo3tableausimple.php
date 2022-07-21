febc197a333d495dd3f9b31dd26f1b47ad60972e
Anthony PARIS <contact@pixel-online.fr>
2022-07-21 16:29:28 +0200

    ok

--- intro/exercice/exo3tableausimple.php
+++ intro/exercice/exo3tableausimple.php
@@ -1,9 +1,10 @@
 en partant du tableau suivant :
 $tab = [10,15,20,10,5,10];
-affiche tous les éléments d'un tableau avec foreach
-calcule la moyenne des nombres contenus dans un tableau donné
+affiche tous les éléments d'un tableau avec foreach en forme de liste ul li
+calcule la moyenne des nombres contenus dans le tableau donné
 indique combien d'éléments sont supérieurs ou égaux à 10
 teste si la note 20 est présente ou non
 détermine la meilleure note obtenue.
 placer la derniere note en premier
 supprimer la premiere note
+trier le tableau du plus petit au plus grand
